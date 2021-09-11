3d8e2a8c9d413c0a36f3eb1f846bd4b442cc2f3b
Leonardo <lm04867@usc.edu>
2021-09-10 17:16:52 -0700

    testing file for lab2

---
+++ lab02_testing.py
@@ -0,0 +1,30 @@
+import sys
+import time
+# By appending the folder of all the GrovePi libraries to the system path here,
+# we are successfully `import grovepi`
+sys.path.append('../../Software/Python/')
+# This append is to support importing the LCD library.
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+
+from grove_rgb_lcd import *
+import grovepi
+
+"""This if-statement checks if you are running this python file directly. That
+is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will
+be true"""
+if __name__ == '__main__':
+    ultrasonic = 4    # D4
+    lcd_display = 2     #i2c2
+    potentiometer = 0   #A0
+
+
+    while True:
+        #So we do not poll the sensors too quickly which may introduce noise,
+        #sleep for a reasonable time of 200ms between each iteration.
+        time.sleep(0.2)
+
+
+
+        distance = grovepi.ultrasonicRead(ultrasonic)
+        setText(distance)
+        setRGB(0,128,64)
